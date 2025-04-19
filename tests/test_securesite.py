7fb928dc4bb70ab82305533c0f94d92a5723ad8b
Eddie Cosma <eddie.cosma@gmail.com>
2025-04-19 13:07:54 -0400

    Added random sleep intervals to file downloads to avoid exceeding rate limits

--- tests/test_securesite.py
+++ tests/test_securesite.py
@@ -1,3 +1,6 @@
+from random import randint
+from time import sleep
+
 import pytest
 
 from medberg import SecureSite
@@ -15,6 +18,7 @@ def test_secure_site_bad_auth():
 
 def test_file_download_by_name(connection, tmp_path):
     test_file_name = connection.files[0].name
+    sleep(randint(4,12))
     connection.get_file(test_file_name, save_dir=tmp_path, save_name="test.txt")
 
     with open(tmp_path / "test.txt") as f:
@@ -23,4 +27,5 @@ def test_file_download_by_name(connection, tmp_path):
 
 def test_file_download_missing_file(connection, tmp_path):
     with pytest.raises(InvalidFileException):
+        sleep(randint(4, 12))
         connection.get_file("not_real.txt", save_dir=tmp_path)
